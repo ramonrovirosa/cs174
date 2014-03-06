918feb99b729abcbf0130007d1678e015ceafb35
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2014-03-05 16:28:08 -0800

    added what needs to be done for emartItems with comments on needed functions

--- src/EmartItems.java
+++ src/EmartItems.java
@@ -6,4 +6,19 @@ public class EmartItems {
             " price INTEGER, " +
             " quantity INTEGER, " +
             " PRIMARY KEY ( stockno ))";
+	
+	//insertEmartItem
+	
+	//update emart items quantity...
+	
+	//maybe also update emart item price....
+	
+	//print all items
+	
+	//remove item by stock#
+	
+	//drop table
+	
 }
+
+
