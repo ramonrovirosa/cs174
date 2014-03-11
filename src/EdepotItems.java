a39789c0c4a020f7f8d524f6df3b4dbf2e73b320
Ramon Rovirosa <ramon.rovirosa@gmail.com>
2014-03-11 00:20:51 -0700

    updated functionality of edopot items

--- src/EdepotItems.java
+++ src/EdepotItems.java
@@ -3,35 +3,58 @@ import java.sql.*;
 
 public class EdepotItems {
 	static String create_table_sql = "CREATE TABLE EdepotItems " +
-            "(stockno INTEGER not NULL, " +
-            " name CHAR(20), " + 
+            "(stockno CHAR(20) not NULL, " +
+            " manufacturer CHAR(20), " +
+            " modelno CHAR(20), " +
+            " min INTEGER, " +
+            " quantity INTEGER, " +
+            " max INTEGER, " +
+            " location CHAR(20), " +
+            " replenishment INTEGER, " +
             " PRIMARY KEY ( stockno ))";
-	//get name by stockno
-	public static void getNamebyStockNO(int stockno, Statement stmt) throws SQLException{
-		ResultSet rs = stmt.executeQuery ("select name from EdepotItems where stockno = "+stockno);
-
-		// Iterate through the result and print the data
-		System.out.println("name where stockno is "+stockno+":");
-		while(rs.next()){
-			// Get the value from column "columnName" with integer type
-			System.out.println(rs.getString("name"));
-		}
-		rs.close();
-	}
+	
+//	//get name by stockno
+//	public static void getNamebyStockNO(int stockno, Statement stmt) throws SQLException{
+//		ResultSet rs = stmt.executeQuery ("select name from EdepotItems where stockno = "+stockno);
+//
+//		// Iterate through the result and print the data
+//		System.out.println("name where stockno is "+stockno+":");
+//		while(rs.next()){
+//			// Get the value from column "columnName" with integer type
+//			System.out.println(rs.getString("name"));
+//		}
+//		rs.close();
+//	}
 	//print whole table
 	public static void printall( Statement stmt) throws SQLException{
 		ResultSet rs = stmt.executeQuery ("select * from EdepotItems");
+		System.out.println("contents of EdepotItems:");
+		System.out.println("stockno,"+
+				   "manufacturer,"+
+				   "modelno,"+
+				   "min,"+
+				   "quantity,"+
+				   "max,"+
+				   "location,"+
+				   "replenishment"+
+				   ") ");
 
 		// Iterate through the result and print the data
-		System.out.println("contents of EdepotItems:");
 		while(rs.next()){
-			// Get the value from column "columnName" with integer type
-			System.out.println("("+rs.getInt("stockno")+","+rs.getString("name")+")");
+			System.out.println(	   rs.getString("stockno").replaceAll("\\s+","")+",    "+
+								   rs.getString("manufacturer").replaceAll("\\s+","")+",         "+
+								   rs.getString("modelno").replaceAll("\\s+","     ")+
+								   rs.getInt("min")+","+
+								   rs.getInt("quantity")+","+
+								   rs.getInt("max")+",     "+
+								   rs.getString("location").replaceAll("\\s+","")+","+
+								   rs.getInt("replenishment")
+				);
 		}
 		rs.close();
 	}
 	//remove by stockno
-	public static void removebystockno(int stockno, Statement stmt){
+	public static void removebystockno(String stockno, Statement stmt){
 		String sql = "DELETE FROM EdepotItems WHERE stockno = "+stockno;
 		try{
 			stmt.executeUpdate(sql);
@@ -43,18 +66,37 @@ public class EdepotItems {
 	   }
 	}
 	//Insert new item into table
-	public static void insertEdepotItem(int stockno, String name, Statement stmt){
-		String sql = "INSERT INTO EdepotItems VALUES ("+stockno+", '"+name+"')";
+	public static void insertEdepotItem(String stockno, String manufacturer, String modelno, 
+										int min, int quantity, int max,
+										String location, int replenishment, Statement stmt){
+		String sql = "INSERT INTO EdepotItems ("+
+												   "stockno,"+
+												   "manufacturer,"+
+												   "modelno,"+
+												   "min,"+
+												   "quantity,"+
+												   "max,"+
+												   "location,"+
+												   "replenishment"+
+											") "+
+										"Values (" + "'" + stockno + "',"
+										   + "'" + manufacturer + "'," 
+										   + "'" + modelno + "',"
+										   + "'" + min + "',"
+										   + "'" + quantity + "',"
+										   + "'" + max + "',"
+										   + "'" + location + "',"
+										   + "'" + replenishment + "')";	
 		try{
 			stmt.executeUpdate(sql);
-			System.out.println("Inserted "+name+" into the database");
+			System.out.println(sql);
 		}catch(SQLException se){
 	      //Handle errors for JDBC
 		  System.out.println(se);
 	      se.printStackTrace();
 	   }
 	}
-	public static void updateEdepotQuantity(int stockno, int quantity, Statement stmt){
+	public static void updateEdepotQuantity(String stockno, int quantity, Statement stmt){
 		String sql = "Update EdepotItems "+
 					 "SET quantity='" + quantity + "' " +
 					 "Where "+ " stockno='"+stockno +"'";
@@ -68,10 +110,10 @@ public class EdepotItems {
 	   }
 	}
 	
-	public static void updateEdepotReplenishment(int stockno, int replenishment, Statement stmt){
+	public static void updateEdepotReplenishment(String stockno, int replenishment, Statement stmt){
 		String sql = "Update EdepotItems "+
-					 "SET quantity='" + replenishment + "' " +
-					 "Where "+ " replenishment='"+replenishment +"'";
+					 "SET replenishment='" + replenishment + "' " +
+					 "Where "+ " stockno='"+stockno +"'";
 		try{
 			stmt.executeUpdate(sql);
 			System.out.println("Updated replenishment for "+stockno+" in the database");
@@ -84,9 +126,9 @@ public class EdepotItems {
 	
 	//drop the EdepotItems table
 	public static void dropEdepotItem(Statement stmt){
-		System.out.println("dropped edepotitems table");
 		try{
 			stmt.executeUpdate("drop table EdepotItems");
+			System.out.println("dropped edepotitems table");
 		}catch(SQLException se){
 		      //Handle errors for JDBC
 			  System.out.println(se);
--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class Main {
 	static Connection conn;
 	static String strConn = "jdbc:oracle:thin:@uml.cs.ucsb.edu:1521:xe";
-	static String strUsername = "alexander_simes";
-	static String strPassword = "4998837";
+	static String strUsername = "ramonrovirosa";
+	static String strPassword = "4935854";
 	public static void main(String[] args) throws SQLException, IOException {
 		// 1. Load the Oracle JDBC driver for this program
 		try {
@@ -30,8 +30,26 @@ public class Main {
 		//EmartPreviousOrders.dropEmartPreviousOrders(stmt);
 		//createTable(EmartPreviousOrders.create_table_sql,stmt);
 		//createTable(EmartAccessories.create_table_sql, stmt);
-		EmartItems.printallformatted(stmt);
-		ConsoleUI.initialPrompt(stmt);
+//		EmartItems.printallformatted(stmt);
+//		ConsoleUI.initialPrompt(stmt);
+		
+//		EdepotItems.dropEdepotItem(stmt);
+//		createTable(EdepotItems.create_table_sql,stmt);
+//		EdepotItems.insertEdepotItem("101", "HP",       "6111", 1,2,10,"A9",  0,stmt);
+//		EdepotItems.insertEdepotItem("201", "Dell",     "420", 	2,3,15,"A7",  0,stmt);
+//		EdepotItems.insertEdepotItem("202", "Emachine", "3958",	2,4,8, "B52", 0,stmt);
+//		EdepotItems.insertEdepotItem("301", "Envision", "720", 	3,4,6, "C27", 0,stmt);
+//		EdepotItems.insertEdepotItem("302", "Samsung",  "712", 	3,4,6, "C13", 0,stmt);
+//		EdepotItems.insertEdepotItem("401", "Symantec", "2005", 5,7,9, "D27", 0,stmt);
+//		EdepotItems.insertEdepotItem("402", "Mcafee",   "2005", 5,7,9, "D1",  0,stmt);
+//		EdepotItems.insertEdepotItem("501", "HP", 		"1320", 2,3,5, "E7",  0,stmt);
+//		EdepotItems.insertEdepotItem("601", "HP", 		"435", 	2,3,9, "F9",  0,stmt);
+//		EdepotItems.insertEdepotItem("602", "Cannon", 	"738", 	2,3,5, "F3",  0,stmt);
+		
+//		EdepotItems.updateEdepotQuantity("101", 2, stmt);
+//		EdepotItems.printall(stmt);
+//		EdepotItems.updateEdepotReplenishment("101", 500, stmt);
+		
 		conn.close();
 	}
 	
